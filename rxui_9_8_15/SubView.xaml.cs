4330088881a24f152eb2c81a7ef067258ed65019
Michael Kreich <michaelkreich@hotmail.com>
2019-01-24 15:44:17 +0100

    added DummyProperty

--- rxui_9_8_15/SubView.xaml.cs
+++ rxui_9_8_15/SubView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ReactiveUI;
+﻿using Windows.UI.Xaml;
+using ReactiveUI;
 
 namespace rxui_9_8_15
 {
@@ -15,8 +16,18 @@ namespace rxui_9_8_15
 
 			this.WhenActivated(disposable =>
 			{
-				TheTextBlock.Text = ViewModel == null ? "ViewModel is null" : "ViewModel is present";
+				TheViewModelTextBlock.Text = ViewModel == null ? "ViewModel is null" : "ViewModel is present";
+				TheDummyPropertyTextBlock.Text = DummyProperty == null ? "DummyProperty is null" : $"DummyProperty is {DummyProperty}";
 			});
+		}
+
+		public static readonly DependencyProperty DummyPropertyProperty = DependencyProperty.Register(
+			"DummyProperty", typeof(string), typeof(SubView), new PropertyMetadata(default(string)));
+
+		public string DummyProperty
+		{
+			get { return (string) GetValue(DummyPropertyProperty); }
+			set { SetValue(DummyPropertyProperty, value); }
 		}
 	}
 
